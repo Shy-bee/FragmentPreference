c2f99d302cad94adc0352277d795816a623f6f8c
Shy-bee <1371470104@qq.com>
2018-05-01 19:48:15 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# 实现设置Activity #
+
+## ![](https://i.imgur.com/hhmQ8uE.png) ##
+
+## ![](https://i.imgur.com/NTKA58g.png) ##
+
+## ![](https://i.imgur.com/VXdI5kC.png) ##
+
+## ![](https://i.imgur.com/Penc4g6.png) ##
+
+## ![](https://i.imgur.com/3BYKYf2.png) ##
+
+## ![](https://i.imgur.com/poYARcP.png) ##
+
---
+++ app/src/main/java/com/example/ykp/fragmentpreference/Screenpreference.java
@@ -0,0 +1,13 @@
+package com.example.ykp.fragmentpreference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screenpreference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screenpreference);
+    }
+}
